c13d1ea9842b4532e1804e498225114cf1645799
wikey10 <vigneshmessi107@gmail.com>
2025-03-12 13:20:22 +0530

    "api key changed"

--- app/src/main/java/com/app/jetweatherforcast/utils/Constants.kt
+++ app/src/main/java/com/app/jetweatherforcast/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.app.jetweatherforcast.utils
 object Constants {
 
     const val BaseURL = "https://api.openweathermap.org/"
-    const val ApiKey = "132a70749571cdd3cec073e24ebfcbf3"
+    const val ApiKey = "YOUR_API_KEY_HERE"
 
 }
